522d4bce1d2a808c94dcd4ebee72821e8f35eeaf
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-04 00:51:23 +0100

    Create test.jsx

---
+++ frontend/src/test.jsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div style={{padding: '20px', background: 'black', color: 'white'}}>
+      <h1>Basic Test</h1>
+      <p>If you see this, React is working</p>
+    </div>
+  );
+}
