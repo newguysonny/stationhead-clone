1975f154576804dc23e3abb6caef91947a87327e
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-04 01:02:39 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom/client'; 
-import App from './App';
+/*import App from './App'; */
 import './App.css';
-
+import Test from './Test'; // Directly import Test component
 
 /*
 const root = ReactDOM.createRoot(document.getElementById('root'));
