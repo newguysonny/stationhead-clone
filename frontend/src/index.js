4c2ca220623aa161be6533088a817fcb09fdaaa0
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-04 01:06:59 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react'; 
 import ReactDOM from 'react-dom/client'; 
 import App from './App'; 
@@ -10,4 +11,15 @@ ReactDOM.render(
     </div>
   </React.StrictMode>,
   document.getElementById('root')
+);
+*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Test from './Test';  // ← Change this from App to Test
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Test />  {/* Now rendering Test instead of App */}
+  </React.StrictMode>
 );
