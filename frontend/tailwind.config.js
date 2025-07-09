894b654fe33bd651563eb380139382ffb41c5f22
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-09 01:17:48 +0100

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+     content: [
+       // Scan all JS/JSX/TS/TSX files in src
+       "./src/**/*.{js,jsx,ts,tsx}", 
+       // Include HTML if using files in public/
+       "./public/**/*.html" 
+     ],
+     theme: {
+       extend: {},
+     },
+     plugins: [],
+   }
