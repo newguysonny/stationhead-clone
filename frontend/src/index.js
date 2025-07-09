c174232b2758240fc2eab2cb6d3403147c1d6866
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-09 01:04:10 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import App from './App'; // Note the curly braces
 import './App.css';
+import './styles/tailwind.output.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
