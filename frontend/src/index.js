f781fb65dc1eba05107c450c19087859f2dfbea7
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-09 02:53:16 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom/client'; 
 import App from './App'; // Note the curly braces
-import './App.css';
+import './App.min.css';
 import './styles/tailwind.output.css';
 
 
