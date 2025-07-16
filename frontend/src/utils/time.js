8c7a821b88adc4b7f124daa660ca28fdda86caf3
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-16 23:17:33 +0100

    Create time.js

---
+++ frontend/src/utils/time.js
@@ -0,0 +1,5 @@
+export const msToMinutes = (ms) => {
+  const minutes = Math.floor(ms / 60000);
+  const seconds = ((ms % 60000) / 1000).toFixed(0);
+  return `${minutes}:${seconds.padStart(2, '0')}`;
+};
