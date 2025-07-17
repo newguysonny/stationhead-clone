6400ff3ee60e5b71854563071b7b0e6491db9eb3
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-17 11:56:44 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import LoginPage from "./pages/LoginPage";
 import CreateRoomPage from "./pages/CreateRoomPage";
 import RoomPage from "./pages/RoomPage";
@@ -21,6 +23,22 @@ export default function App() {
         <Route path="/room/:roomName" element={<StreamingRoomWrapper />} />
       </Routes>
     </BrowserRouter>
+
+     {/* Toast Container - placed outside BrowserRouter but in the same fragment */}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
+    
   );
 }
 
